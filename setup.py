7484441c23654484943894e909d35767ac12c682
Impact <support@gugy.eu>
2020-12-04 11:03:27 +0100

    Fix typo in plugin description

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Automatically focuses the command input wehn switching to the Terminal tab."""
+plugin_description = """Automatically focuses the command input when switching to the Terminal tab."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "jneilliii"
